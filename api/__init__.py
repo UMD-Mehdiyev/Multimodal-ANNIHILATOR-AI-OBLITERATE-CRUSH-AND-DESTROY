038bfd5618cce1b802f3f0ad8d714d7570a4725b
mehdi <mehdiyev.ayyhan@gmail.com>
2024-07-10 18:00:00 -0400

    Create API calls for simple user base

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,13 @@
+from flask import Flask
 from firebase_admin import credentials, initialize_app
 
 cred = credentials.Certificate("api/serviceAccountKey.json")
 default_app = initialize_app(cred)
+
+def create_app():
+    app = Flask(__name__)
+    
+    from .userAPI import userAPI
+    
+    app.register_blueprint(userAPI, url_prefix='/user')
+    return app
---
+++ api/userAPI.py
@@ -0,0 +1,25 @@
+import uuid
+from flask import Blueprint, request, jsonify
+from firebase_admin import firestore
+
+db = firestore.client()
+user_Ref = db.collection('user')
+
+userAPI = Blueprint('userAPI', __name__)
+
+@userAPI.route('/add', methods=['POST'])
+def create():
+    try:
+        id = uuid.uuid4()
+        user_Ref.document(id.hex).set(request.json)
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An error occured: {e}"
+    
+@userAPI.route('/list')
+def read():
+    try:
+        all_users = [doc.to_dict() for doc in user_Ref.stream()]
+        return jsonify(all_users), 200  
+    except Exception as e:
+        return f"An error occured: {e}"
